1144b2352c4cbfec226517fdcf43cde606a1c787
pirhamahar <pirhamahar066@gmail.com>
2026-08-28 21:19:25 +0500

    Add Gradebook class to manage students

--- src/gradebook/gradebook.py
+++ src/gradebook/gradebook.py
@@ -3,3 +3,15 @@ class Student:
         self.name = name
         self.roll_no = roll_no
         self.scores = []
+
+class Gradebook:
+    def __init__(self):
+        self.students = []
+
+    def add_student(self, name, roll_no):
+        for student in self.students:
+            if student.roll_no == roll_no:
+                return False
+
+        self.students.append(Student(name, roll_no))
+        return True
