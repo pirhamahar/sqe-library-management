e94b82b72c18c4450182d58c66ef7c26363ea7db
pirhamahar <pirhamahar066@gmail.com>
2026-09-15 15:21:24 +0500

    Add ISBN boundary tests
    Add tests for ISBN validation length boundaries.

--- tests/test_validate_isbn.py
+++ tests/test_validate_isbn.py
@@ -13,3 +13,16 @@ def test_valid_isbn():
 def test_invalid_isbn_classes(isbn):
     with pytest.raises(ValueError):
         validate_isbn(isbn)
+
+@pytest.mark.parametrize("length", [11, 12, 14, 15])
+def test_validate_isbn_length_boundaries_invalid(length):
+    isbn = "1" * length
+
+    with pytest.raises(ValueError):
+        validate_isbn(isbn)
+
+
+def test_validate_isbn_length_boundary_valid():
+    isbn = "1" * 13
+
+    assert validate_isbn(isbn) is True
